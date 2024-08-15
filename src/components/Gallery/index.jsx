7455485bf492871598e37dd4f1516810b244c05e
lassejlv <vestergaardlasse2@gmail.com>
2024-08-15 09:15:11 +0200

    changes

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,14 +1,16 @@
+import styles from "./index.module.scss";
+
 export default function Gallery() {
   return (
-    <>
-      {Array.from({ length: 13 }).map((_, index) => (
-        <div key={index} className="gallery-item">
+    <div className={styles.galleryGrid}>
+      {Array.from({ length: 12 }).map((_, index) => (
+        <div key={index} className={styles.galleryItem}>
           <img
             src={`/gallery/image${index + 1}.jpeg`}
             alt={`Gallery item ${index + 1}`}
           />
         </div>
       ))}
-    </>
+    </div>
   );
 }
